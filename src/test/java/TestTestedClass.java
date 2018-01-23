513942d473b3681d4133d7a70bf4de98615ab43a
usmaha <Net#ork19>
2018-01-23 17:25:42 +0200

    initial commit

---
+++ src/test/java/TestTestedClass.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTestedClass {
+
+    @Test
+    public void testPositive() {
+        TestedClass testedClass = new TestedClass();
+        Assert.assertTrue(testedClass.checkPositive(1));
+    }
+
+    @Test
+    public void testDivideByTwo() {
+        TestedClass testedClass = new TestedClass();
+        Assert.assertTrue(testedClass.checkIfDividableByTwo(6));
+    }
+}
